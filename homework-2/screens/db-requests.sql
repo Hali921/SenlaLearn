35fbc5d1a50344e1b0dd663e334565c0a1791342
Ildar Khaliullin <hali92@list.ru>
2024-08-01 12:05:00 +0300

    add screen files

--- homework-2/screens/db-requests.sql
+++ homework-2/screens/db-requests.sql
@@ -5,7 +5,7 @@ ON reviews.parts_id = parts.id;
 
 SELECT clients.firstname, lastname FROM clients
 INNER JOIN orders
-ON orders.clients_id = clients.id
+ON orders.client_id = clients.id
 WHERE orders.orders_date = "01-07-2024";
 
 
